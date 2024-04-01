506e0306a0409ed6443e8939e471f27b0968c2bc
Kerubananthan-Ganesan <104969840+Kerubananthan3761@users.noreply.github.com>
2024-04-01 13:09:12 +0530

    Added tree map levels sample

---
+++ Levels/Levels/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Levels
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Levels/Levels/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Levels
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
